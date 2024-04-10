d03c77ebe36475eea09e59501ff09fde2175464c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-10 05:08:41 -0500

    feat (HeroesRoute.jsx): creación de rutas para search y hero

--- src/modules/heroes/routes/HeroesRoutes.jsx
+++ src/modules/heroes/routes/HeroesRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Navbar } from "../../../ui";
-import { MarvelPage, DcPage } from '../pages';
+import { MarvelPage, DcPage, SearchPage, HeroPage } from '../pages';
 
 export const HeroesRoutes = () => {
     return (
@@ -10,6 +10,8 @@ export const HeroesRoutes = () => {
                 <Routes>
                     <Route path='marvel' element={<MarvelPage />} />
                     <Route path='dc' element={<DcPage />} />
+                    <Route path='search' element={<SearchPage />} />
+                    <Route path='hero' element={<HeroPage />} />
 
                     <Route path='/' element={<Navigate to='marvel' />} />
                 </Routes>
