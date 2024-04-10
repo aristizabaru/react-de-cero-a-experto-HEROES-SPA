76d51e731cdf295a75297b94528cdcd119c226e0
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-09 19:24:56 -0500

    fix (main.jsx): carga de componente HeroesApp como inicio de la SPA

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles.css';
+import { HeroesApp } from './HeroesApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hola</h1>
-  </React.StrictMode>,
+    <HeroesApp />
+  </React.StrictMode >,
 );
