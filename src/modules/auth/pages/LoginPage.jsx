6f47bff1121275321c01f5b136820debd4b51c24
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-10 05:29:43 -0500

    feat (loginPage.jsx): implementación de login
    Implementa btn para navegar de vuelta al home. No se implementa protección de rutas

--- src/modules/auth/pages/LoginPage.jsx
+++ src/modules/auth/pages/LoginPage.jsx
@@ -1,5 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
 export const LoginPage = () => {
+
+    const navigate = useNavigate();
+
+    const onLogin = () => {
+        navigate('/', {
+            replace: true,
+        });
+    };
+
     return (
-        <h1>LoginPage</h1>
+        <div className="container mt-5">
+            <h1>LoginPage</h1>
+            <hr />
+            <button
+                className="btn btn-secondary"
+                onClick={onLogin}
+            >
+                Login
+            </button>
+        </div>
     );
 };
