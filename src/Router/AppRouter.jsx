572c30d86c8bbfec67378f4d1c63f420046b12c4
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-09 20:26:27 -0500

    feat (AppRouter.jsx): adición del componente Navbar

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,17 +1,22 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { MarvelPage } from '../modules/heroes/pages/MarvelPage';
-import { DcPage } from '../modules/heroes/pages/DcPage';
-import { LoginPage } from '../modules/auth/pages/LoginPage';
+import { Navbar } from '../ui';
+import { MarvelPage, DcPage } from '../modules/heroes';
+import { LoginPage } from '../modules/auth';
+
 
 export const AppRouter = () => {
     return (
-        <Routes>
-            <Route path='marvel' element={<MarvelPage />} />
-            <Route path='dc' element={<DcPage />} />
+        <>
+            <Navbar />
+            <Routes>
+                <Route path='marvel' element={<MarvelPage />} />
+                <Route path='dc' element={<DcPage />} />
 
-            <Route path='login' element={<LoginPage />} />
+                <Route path='login' element={<LoginPage />} />
 
-            <Route path='/' element={<Navigate to='marvel' />} />
-        </Routes>
+                <Route path='/' element={<Navigate to='marvel' />} />
+            </Routes>
+        </>
+
     );
 };
