d50309ddabb61041a7eae003acc25f014a1cfad5
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-09 20:00:19 -0500

    feat (main.jsx): add BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { BrowserRouter } from 'react-router-dom';
+
 import './styles.css';
 import { HeroesApp } from './HeroesApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HeroesApp />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </React.StrictMode >,
 );
