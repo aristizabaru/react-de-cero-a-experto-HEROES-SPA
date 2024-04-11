5a930ec469fe6f8a2192447debc96b58fb226035
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-11 14:08:36 -0500

    feat (DcPage.jsx): implementación de página DC

--- src/modules/heroes/pages/DcPage.jsx
+++ src/modules/heroes/pages/DcPage.jsx
@@ -1,5 +1,11 @@
+import { HeroList } from "../components/HeroList";
+
 export const DcPage = () => {
     return (
-        <h1>DcPage</h1>
+        <>
+            <h1>DC Comics</h1>
+            <hr />
+            <HeroList publisher='DC Comics' />
+        </>
     );
 };
