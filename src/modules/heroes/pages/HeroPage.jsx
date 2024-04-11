f24784af9fc2efcd2d4bd72f58502ce2b3eb1b05
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-11 14:16:22 -0500

    feat (HeroPage.jsx): implementación del single de Hero
    Captura parámetros de la URL para traer información del Heroe y pinta los datos

--- src/modules/heroes/pages/HeroPage.jsx
+++ src/modules/heroes/pages/HeroPage.jsx
@@ -1,5 +1,46 @@
+import { useMemo } from 'react';
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
+import { getHeroesById } from '../helpers';
+
 export const HeroPage = () => {
+
+    // Custom hook de react router para tomar los parámetros de la URL
+    const { id, ...url } = useParams();
+    const navigate = useNavigate();
+    const hero = useMemo(() => getHeroesById(id), [id]);
+
+    const onNavigateBack = () => {
+        // retorna a la última página visitada usando el historial de navegación
+        // navigate(-1);
+        const publisher = url['*'].split('/')[1].split('-')[0];
+        navigate(`/${publisher}`);
+    };
+
+    // Redirecciona a /marvel si la ruta no es valida, si el id no se encuentra
+    if (!hero) {
+        return <Navigate to='/marvel' />;
+    }
     return (
-        <h1>HeroPage</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img src={`/assets/heroes/${id}.jpg`} alt={hero.superhero} className='img-thumbnail' />
+            </div>
+            <div className="col-8">
+                <h1>{hero.superhero}</h1>
+                <ul className='list-group list-group-flush'>
+                    <li className='list-group-item'><b>Alter ego: </b>{hero.alter_ego}</li>
+                    <li className='list-group-item'><b>Publisher: </b>{hero.publisher}</li>
+                    <li className='list-group-item'><b>First Appearance: </b>{hero.first_appearance}</li>
+                </ul>
+                <h5 className='mt-3'>Characters</h5>
+                <p>{hero.characters}</p>
+                <button
+                    className='btn btn-outline-secondary mt-4'
+                    onClick={onNavigateBack}
+                >
+                    Regresar
+                </button>
+            </div>
+        </div>
     );
 };
