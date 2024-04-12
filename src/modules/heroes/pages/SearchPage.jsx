b90428bd1d1fa4baf078bf90d183c52786bfc15e
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-12 04:38:24 -0500

    feat (SearchPage.jsx): implementación de página de búsqueda
    Formulario de búsqueda toma parámetros de URL para carga de herores

--- src/modules/heroes/pages/SearchPage.jsx
+++ src/modules/heroes/pages/SearchPage.jsx
@@ -1,5 +1,67 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import { useForm } from "../../../hooks/useForm";
+import { getHeroesByName } from '../helpers';
+import { HeroCard } from "../components/HeroCard";
+
 export const SearchPage = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const { q = '' } = queryString.parse(location.search);
+    const heroes = getHeroesByName(q);
+
+    const { searchText, onInputChange } = useForm({
+        searchText: q,
+    });
+
+    const onSearchSubmit = (event) => {
+        event.preventDefault();
+        if (searchText.trim().length <= 1) return;
+
+        console.log({ searchText });
+        navigate(`?q=${searchText.toLowerCase().trim()}`);
+    };
+
     return (
-        <h1>SearchPage</h1>
+        <>
+            <h1>Search</h1>
+            <hr />
+            <div className="row">
+                <div className="col-5">
+                    <h4>Searching</h4>
+                    <hr />
+                    <form className="mt-2" onSubmit={onSearchSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Search a hero"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                            value={searchText}
+                            onChange={onInputChange}
+                        />
+                        <button className="btn btn-secondary mt-3">Search</button>
+                    </form>
+                </div>
+                <div className="col-7">
+                    <h4>Results</h4>
+                    <hr />
+                    <div className="alert alert-primary">
+                        Search a hero
+                    </div>
+                    <div className="alert alert-danger">
+                        There&#39;s no results with <b>{q}</b>
+                    </div>
+                    {
+                        heroes.map(hero => (
+                            <HeroCard key={hero.id} {...hero} />
+                        ))
+                    }
+
+                </div>
+            </div>
+        </>
     );
 };
