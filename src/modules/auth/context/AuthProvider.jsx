c4f2557b750517662dbe40409e55c1a5458f92a7
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-12 11:45:38 -0500

    feat (AuthPrivider.jsx): implementación de persistencia de estado en local storage

--- src/modules/auth/context/AuthProvider.jsx
+++ src/modules/auth/context/AuthProvider.jsx
@@ -6,28 +6,36 @@ import { authReducer } from "./authReducer";
 
 import { types } from "../types/types";
 
-const initialState = {
-    logged: false,
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user,
+    };
 };
 
 export const AuthProvider = ({ children }) => {
 
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, {}, init);
 
     const login = (name = '') => {
 
+        const user = { id: 'ABC', name, };
+
         const action = {
             type: types.login,
-            payload: name,
+            payload: user,
         };
 
+        localStorage.setItem('user', JSON.stringify(user));
         dispatch(action);
     };
 
     return (
         // La prop value expone los datos que se compartirán en el contexto
         <AuthContext.Provider value={{
-            authState,
+            ...authState,
             login: login
         }}>
             {children}
