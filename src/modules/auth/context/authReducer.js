c7e193b235c80749da5c585b6516013f0ea6e04e
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-12 05:38:36 -0500

    feat (authReducer.js): implementación del reducer
    Manejo de los estados de auth de la app

---
+++ src/modules/auth/context/authReducer.js
@@ -0,0 +1,20 @@
+import { types } from "../types/types";
+
+export const authReducer = (state = {}, action) => {
+    switch (action.type) {
+        case types.login:
+            return {
+                ...state,
+                logged: true,
+                name: action.payload,
+            };
+        case types.logout:
+            return {
+                ...state,
+                logged: false,
+            };
+
+        default:
+            return state;
+    }
+};
