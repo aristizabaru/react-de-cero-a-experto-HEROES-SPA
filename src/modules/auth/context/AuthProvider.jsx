e83451d84dc0d643c480272a5c4b6c0af67f866b
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-12 05:37:58 -0500

    feat (AuthProvider.jsx): implementación del AuthProvider
    Maneja el estado global por medio de useReducer

---
+++ src/modules/auth/context/AuthProvider.jsx
@@ -0,0 +1,20 @@
+// Recibe los componentes hijos en las props. Esto lo hace un high order component
+
+import { useReducer } from "react";
+import { AuthContext } from "./AuthContext";
+import { authReducer } from "./authReducer";
+
+const initialState = {
+    logged: false,
+};
+
+export const AuthProvider = ({ children }) => {
+
+    const [authState, dispatch] = useReducer(authReducer, initialState);
+
+    return (
+        <AuthContext.Provider value={{}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
