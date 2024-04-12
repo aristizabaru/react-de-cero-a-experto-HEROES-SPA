a153bc5f846dfdba534ee6d54fedbd6ec4780849
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-12 11:46:10 -0500

    style (authReducer.js): adición de saltos de línea para mejor legibilidad

--- src/modules/auth/context/authReducer.js
+++ src/modules/auth/context/authReducer.js
@@ -1,6 +1,7 @@
 import { types } from "../types/types";
 
 export const authReducer = (state = {}, action) => {
+
     switch (action.type) {
         case types.login:
             return {
@@ -17,4 +18,5 @@ export const authReducer = (state = {}, action) => {
         default:
             return state;
     }
+
 };
