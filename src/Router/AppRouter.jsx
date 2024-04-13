9d5feb2671d262242fccd5756e3c9bd4b2ef747b
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-13 08:43:34 -0500

    feat (AppRouter.jsx): se crea protección de rutas usando un High Order Component

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,15 +1,31 @@
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from '../modules/auth';
 import { HeroesRoutes } from '../modules/heroes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
     return (
         <Routes>
-            <Route path='login' element={<LoginPage />} />
+            <Route path='login' element={
+                <PublicRoute>
+                    <LoginPage />
+                </PublicRoute>
+            } />
 
-            {/* {uso de wildcard para que cualquier ruta que no sea login
-                    cargue el router de heroes} */}
-            <Route path='/*' element={<HeroesRoutes />} />
+            {
+                /* {uso de wildcard para que cualquier ruta que no sea login
+                    cargue el router de heroes} */
+            }
+            <Route path='/*' element={
+                /* Los high order components siempre envuelven otros componentes
+                   que son pasados como hijos a este */
+                <PrivateRoute>
+                    <HeroesRoutes />
+                </PrivateRoute>
+            } />
+
+            {/* <Route path='login' element={<LoginPage />}/> */}
         </Routes>
     );
 };
