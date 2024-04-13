cfaea934ee135b9e4a7d4aedfdbd53c5cbf0d110
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-12 19:18:14 -0500

    feat (AuthProvider.jsx): implementación de función logout
    Gestiona la actualización del contexto global y remueve el usuario del localStorage

--- src/modules/auth/context/AuthProvider.jsx
+++ src/modules/auth/context/AuthProvider.jsx
@@ -23,12 +23,18 @@ export const AuthProvider = ({ children }) => {
 
         const user = { id: 'ABC', name, };
 
-        const action = {
-            type: types.login,
-            payload: user,
-        };
+        const action = { type: types.login, payload: user, };
 
         localStorage.setItem('user', JSON.stringify(user));
+        dispatch(action);
+    };
+
+    const logout = () => {
+
+        const action = { type: types.logout };
+
+        localStorage.removeItem('user');
+
         dispatch(action);
     };
 
@@ -36,7 +42,8 @@ export const AuthProvider = ({ children }) => {
         // La prop value expone los datos que se compartirán en el contexto
         <AuthContext.Provider value={{
             ...authState,
-            login: login
+            login: login,
+            logout: logout,
         }}>
             {children}
         </AuthContext.Provider>
