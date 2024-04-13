f05a5d2de6c8b629c8c5feddac013226f886f1f8
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-13 08:42:02 -0500

    feat (LoginPage.jsx): se carga última ruta guardada en el login
    La ruta corresponde a la última url visitada por el usuario antes de hacer logout

--- src/modules/auth/pages/LoginPage.jsx
+++ src/modules/auth/pages/LoginPage.jsx
@@ -9,9 +9,11 @@ export const LoginPage = () => {
 
     const onLogin = () => {
 
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
         login('Andrés Aristizábal');
 
-        navigate('/', {
+        navigate(lastPath, {
             replace: true,
         });
     };
